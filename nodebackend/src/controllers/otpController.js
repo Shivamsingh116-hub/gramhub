abbedace43dc203435962492889e98cae5fd2fc6
Shivamsingh116-hub <shivamsinghk116@gmail.com>
2025-09-30 11:41:15 +0530

    update otp

--- nodebackend/src/controllers/otpController.js
+++ nodebackend/src/controllers/otpController.js
@@ -25,6 +25,7 @@ const sendOtp = async (req, res) => {
         const transporter = nodemailer.createTransport({
             service: 'gmail',
             port:587,
+            secure:false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
